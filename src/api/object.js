d7612894c9ac54d254252d56d619d24d13d60a47
蚍蜉 <jpx1024@gmail.com>
2021-05-07 13:15:07 +0800

    完成上传功能

--- src/api/object.js
+++ src/api/object.js
@@ -1,4 +1,5 @@
 import request from '@/utils/request'
+import { fileServerURL } from '@/config'
 
 const BASE_PATH = '/object';
 
@@ -18,15 +19,58 @@ export function createFolder(data) {
     })
 }
 
-export function uploadObject(params){
+export function getPresignedFormData(params) {
     return request({
-        url: BASE_PATH + '/upload',
+        url: BASE_PATH + '/presigned/formdata',
+        method: 'get',
+        params
+    })
+}
+
+export function getPresignedUrl(params) {
+    return request({
+        url: BASE_PATH + '/presigned/url',
+        method: 'get',
+        params
+    })
+}
+
+export function createMultipartUpload(params) {
+    return request({
+        url: BASE_PATH + '/uploadmultipart',
         method: 'post',
         params
     })
 }
 
-export function removeObject(params){
+export function composeUploadPart(data) {
+    return request({
+        url: BASE_PATH + '/upload/part/compose',
+        method: 'post',
+        data
+    })
+}
+
+export function getUploadPart(params) {
+    return request({
+        url: BASE_PATH + '/upload/part',
+        method: 'get',
+        params
+    })
+}
+
+export function uploadObjectToServer(bucketName, data) {
+    return request({
+        url: fileServerURL + '/' + bucketName,
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data
+    })
+}
+
+export function removeObject(params) {
     return request({
         url: BASE_PATH,
         method: 'delete',
--- src/views/object/components/UploadDialog.vue
+++ src/views/object/components/UploadDialog.vue
@@ -1,27 +1,15 @@
 <template>
-	<el-dialog title="上传文件" :visible.sync="dialogVisible" :before-close="handleClose">
-		<el-form ref="form" :model="form" label-width="100px">
+	<el-dialog title="提示" :visible.sync="dialogVisible" :before-close="handleClose">
+		<el-form ref="form" :model="form" label-width="80px">
 			<el-form-item label="上传至">
 				<el-radio-group v-model="uploadIn">
 					<el-radio :label="0" border>默认目录</el-radio>
 					<el-radio :label="1" border>指定目录</el-radio>
 				</el-radio-group>
 
-				<el-input v-model="form.path" v-if="uploadIn === 1">
-					<template slot="prepend">{{ bucketURL }}</template>
+				<el-input v-model="form.prefix" v-if="uploadIn === 1">
+					<template slot="prepend">{{ bucketName }} / </template>
 				</el-input>
-			</el-form-item>
-			<el-form-item label="待上传文件">
-				<uploader :options="options" class="uploader-example">
-					<uploader-unsupport></uploader-unsupport>
-					<uploader-drop>
-						<p><i class="el-icon-upload"></i>将目录或多个文件拖拽到此进行扫描</p>
-						<p>文件命名规范：1.使用 UTF-8 编码 2.区分大小写 3.长度必须在 1~1023 字节之间 4. 不能以 / 或者 \ 字符开头</p>
-						<uploader-btn class="el-button el-button--primary">选取文件</uploader-btn>
-						<uploader-btn class="el-button el-button--primary" :directory="true">选取文件夹</uploader-btn>
-					</uploader-drop>
-					<uploader-list></uploader-list>
-				</uploader>
 			</el-form-item>
 
 			<el-form-item label="存储类型">
@@ -30,109 +18,105 @@
 				</el-radio-group>
 			</el-form-item>
 
-			<el-form-item label="用户元数据">
-				<el-tag type="warning" closable :disable-transitions="false" @close="removeTag(key)" v-for="(val, key, index) in form.userMetaData" :key="index">
-					{{ key }} : {{ val }}
-				</el-tag>
-
-				<el-row class="input-map">
-					<el-col :span="10">
-						<el-input v-model="userMetaDataMap.key" placeholder="请输入键"></el-input>
-					</el-col>
-					<el-col :span="10">
-						<el-input v-model="userMetaDataMap.value" placeholder="请输入值"></el-input>
-					</el-col>
-					<el-col :span="4">
-						<el-button @click="addUserMetadata">添加</el-button>
-					</el-col>
-				</el-row>
+			<el-form-item label="待上传文件">
+				<dashboard :uppy="uppy" :plugins="['Webcam']" />
 			</el-form-item>
 		</el-form>
 
 		<span slot="footer" class="dialog-footer">
 			<el-button @click="dialogVisible = false">取 消</el-button>
-			<el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+			<el-button type="primary">确 定</el-button>
 		</span>
 	</el-dialog>
+
 </template>
+
 <script>
-import { uploadObject } from '@/api/object'
-import { fileServerURL } from '@/config'
+import { createMultipartUpload as createPartUpload, getPresignedUrl, getUploadPart, composeUploadPart } from '@/api/object'
+
+import { Dashboard } from '@uppy/vue'
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+import Uppy from '@uppy/core'
+import Webcam from '@uppy/webcam'	
+import AwsS3Multipart from '@uppy/aws-s3-multipart'
+var Locale = require('@uppy/locales/lib/zh_CN')
 
 export default {
+	components: {
+		Dashboard,
+	},
 	data() {
 		return {
 			dialogVisible: false,
+			bucketName: '',
+			uploadIn: 0,
 			form: {
-				path: '',
+				prefix: '',
 				storageClass: 'STANDARD',
 				userMetaData: {},
 			},
-			userMetaDataMap: {
-				key: '',
-				value: '',
-			},
-			uploadIn: 0,
-			bucketURL: '',
-			uploader: {
-				options: {},
-				btn: {},
-			},
+			formData: {},
 		}
 	},
 	created() {},
+	computed: {
+		uppy: function () {
+			let bucketName = this.bucketName;
+            let prefix = this.form.prefix;
+            
+            return new Uppy({
+                locale: Locale,
+            }).use(Webcam).use(AwsS3Multipart, {
+                createMultipartUpload(file){
+					// 创建部分上传
+                    return createPartUpload({
+						bucketName,
+						key: file.name
+					}).then(res => res.data)
+                },
+                listParts(file, partData){
+                    // 获取上传部分
+					return getUploadPart(Object.assign({
+						bucketName
+					}, partData)).then(res => res.data);
+                },
+				completeMultipartUpload(file, partData) {
+					return getPresignedUrl({
+                        bucketName,
+                        objectName: file.name,
+                        method: 'GET'
+                    }).then(res => {
+                        return {
+                            location: res.data
+                        }
+                    })
+				},
+                prepareUploadPart(file, partData){
+					console.log('签名执行')
+                    let method = 'PUT'
+					// 获取签名Url
+                    return getPresignedUrl({
+                        bucketName,
+                        objectName: file.name,
+                        method
+                    }).then(res => {
+                        return {
+                            url: res.data
+                        }
+                    })
+                }
+            })
+		},
+	},
 	methods: {
 		show(data) {
 			this.dialogVisible = true
-			this.bucketURL = fileServerURL + '/' + data.bucketName
+			this.bucketName = data.bucketName
 		},
 		handleClose(done) {
 			done()
 		},
-		addUserMetadata() {
-			if (
-				this.userMetaDataMap.key !== '' &&
-				this.userMetaDataMap.value !== ''
-			) {
-				this.form.userMetaData[
-					this.userMetaDataMap.key
-				] = this.userMetaDataMap.value
-				this.userMetaDataMap = {
-					key: '',
-					value: '',
-				}
-			} else {
-				this.$message({
-					message: '请输入键值对',
-					type: 'warning',
-				})
-			}
-		},
-		removeUserMetadata(key) {
-			let newUserMetadata = Object.assign({}, this.form.userMetaData)
-			delete newUserMetadata[key]
-			this.form.userMetaData = newUserMetadata
-		},
 	},
 }
 </script>
-
-<style scoped>
-.upload-demo {
-	width: 100%;
-}
-
-.uploader-example {
-	font-size: 12px;
-	/* box-shadow: 0 0 10px rgba(0, 0, 0, 0.4); */
-}
-.uploader-example .uploader-btn {
-	margin-right: 4px;
-}
-.uploader-example .uploader-list {
-	max-height: 440px;
-	overflow: auto;
-	overflow-x: hidden;
-	overflow-y: auto;
-}
-</style>
